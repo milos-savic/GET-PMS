7ffd961a3f08fee2f9c69b9e321b4b5e8f064bf0
Milos Savic <milos.savic@seavus.com>
2016-10-05 10:38:54 +0200

    GET-PMS is the root of maven project hierarchy

---
+++ infrastructure/liquibase/src/main/java/by/get/pms/config/LiquibaseConfiguration.java
@@ -0,0 +1,13 @@
+package by.get.pms.config;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+/**
+ * Created by milos.savic on 10/5/2016.
+ */
+
+public class LiquibaseConfiguration {
+
+
+}
---
+++ infrastructure/spring-jpa/src/main/java/by/get/pms/config/SpringJpaDataSourceConfig.java
@@ -0,0 +1,7 @@
+package by.get.pms.config;
+
+/**
+ * Created by milos.savic on 10/5/2016.
+ */
+public class SpringJpaDataSourceConfig {
+}
