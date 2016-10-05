5a84c54bc3087561c06dedc8dc7a7ce60f96f93b
Milos Savic <milos.savic@seavus.com>
2016-10-05 17:33:55 +0200

    start work on index, fragments, sign-in

---
+++ dist/pms-webapp/src/main/java/by/get/pms/web/controller/WebConstants.java
@@ -0,0 +1,19 @@
+package by.get.pms.web.controller;
+
+/**
+ * Created by milos.savic on 10/5/2016.
+ */
+public final class WebConstants {
+
+	private WebConstants() {
+	}
+
+	public final static String SIGNIN_PAGE = "/signin";
+	public final static String DEFAULT_PAGE = "/";
+
+	public final static String REST_API_URL = "/rest";
+
+	// projects
+	public final static String PROJECTS_URL = "/projects";
+
+}
