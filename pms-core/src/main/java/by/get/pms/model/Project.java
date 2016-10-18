9719d3e11fa580bd66864db41930f152895c36a6
Milos Savic <milos.savic@seavus.com>
2016-10-18 14:38:44 +0200

    @OneToMany relations to the Task list

--- pms-core/src/main/java/by/get/pms/model/Project.java
+++ pms-core/src/main/java/by/get/pms/model/Project.java
@@ -2,6 +2,7 @@ package by.get.pms.model;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * Created by milos on 14-Oct-16.
@@ -11,63 +12,75 @@ import java.io.Serializable;
 @Table(name = "project")
 public class Project extends PersistentEntity {
 
-    @Column(name = "code", unique = true, nullable = false, length = 30)
-    private String code;
+	@Column(name = "code", unique = true, nullable = false, length = 30)
+	private String code;
 
-    @Column(name = "name", nullable = false, length = 50)
-    private String name;
+	@Column(name = "name", nullable = false, length = 50)
+	private String name;
 
-    @Column(name = "description")
-    private String description;
+	@Column(name = "description")
+	private String description;
 
-    @ManyToOne(optional = false)
-    @JoinColumn(name = "project_manager")
-    private User projectManager;
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "project_manager")
+	private User projectManager;
 
-    public String getCode() {
-        return code;
-    }
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "project")
+	private List<Task> tasks;
 
-    public void setCode(String code) {
-        this.code = code;
-    }
+	public String getCode() {
+		return code;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public void setCode(String code) {
+		this.code = code;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public User getProjectManager() {
-        return projectManager;
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    public void setProjectManager(User projectManager) {
-        this.projectManager = projectManager;
-    }
+	public User getProjectManager() {
+		return projectManager;
+	}
 
-    @Override
-    public Serializable getIdentifier() {
-        return getCode();
-    }
+	public void setProjectManager(User projectManager) {
+		this.projectManager = projectManager;
+	}
 
-    @Override
-    public String toString() {
-        return "Project{" +
-                "id= " + getId() + '\'' +
-                "code= '" + code + '\'' +
-                ", name= '" + name + '\'' +
-                ", description= '" + description + '\'' +
-                '}';
-    }
+	public List<Task> getTasks() {
+		return tasks;
+	}
+
+	public void setTasks(List<Task> tasks) {
+		this.tasks = tasks;
+	}
+
+	@Override
+	public Serializable getIdentifier() {
+		return getCode();
+	}
+
+	@Override
+	public String toString() {
+		return "Project{" +
+				"id='" + getId() + '\'' +
+				"code='" + code + '\'' +
+				", name='" + name + '\'' +
+				", description='" + description + '\'' +
+				", projectManager=" + projectManager +
+				'}';
+	}
 }
--- pms-core/src/main/java/by/get/pms/model/User.java
+++ pms-core/src/main/java/by/get/pms/model/User.java
@@ -1,6 +1,7 @@
 package by.get.pms.model;
 
 import javax.persistence.*;
+import java.util.List;
 
 /**
  * Created by Milos.Savic on 10/12/2016.
@@ -10,62 +11,73 @@ import javax.persistence.*;
 @Table(name = "user")
 public class User extends PersistentEntity {
 
-    @Column(name = "firstname", nullable = false)
-    private String firstName;
+	@Column(name = "firstname", nullable = false)
+	private String firstName;
 
-    @Column(name = "lastname", nullable = false)
-    private String lastName;
+	@Column(name = "lastname", nullable = false)
+	private String lastName;
 
-    @Column(name = "email", nullable = false)
-    private String email;
+	@Column(name = "email", nullable = false)
+	private String email;
 
-    @OneToOne(optional = false, cascade = CascadeType.ALL,
-            mappedBy = "user", targetEntity = UserAccount.class)
-    private UserAccount userAccount;
+	@OneToOne(optional = false, cascade = CascadeType.ALL,
+			mappedBy = "user", targetEntity = UserAccount.class)
+	private UserAccount userAccount;
 
-    public UserAccount getUserAccount() {
-        return userAccount;
-    }
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "assignee")
+	private List<Task> assignedTasks;
 
-    public void setUserAccount(UserAccount userAccount) {
-        this.userAccount = userAccount;
-    }
+	public UserAccount getUserAccount() {
+		return userAccount;
+	}
 
-    public String getLastName() {
-        return lastName;
-    }
+	public void setUserAccount(UserAccount userAccount) {
+		this.userAccount = userAccount;
+	}
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+	public String getLastName() {
+		return lastName;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public String getFirstName() {
-        return firstName;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+	public String getFirstName() {
+		return firstName;
+	}
 
-    public String getFullName() {
-        return getFirstName() + " " + getLastName();
-    }
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "id= " + getId() + '\'' +
-                "firstName= '" + firstName + '\'' +
-                ", lastName= '" + lastName + '\'' +
-                ", email= '" + email + '\'' +
-                '}';
-    }
+	public String getFullName() {
+		return getFirstName() + " " + getLastName();
+	}
+
+	public List<Task> getAssignedTasks() {
+		return assignedTasks;
+	}
+
+	public void setAssignedTasks(List<Task> assignedTasks) {
+		this.assignedTasks = assignedTasks;
+	}
+
+	@Override
+	public String toString() {
+		return "User{" +
+				"id= " + getId() + '\'' +
+				"firstName= '" + firstName + '\'' +
+				", lastName= '" + lastName + '\'' +
+				", email= '" + email + '\'' +
+				'}';
+	}
 }
