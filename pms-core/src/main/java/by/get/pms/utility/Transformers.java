c051924f364658001cd86e7e908013036d985bf8
Milos Savic <milos.savic@seavus.com>
2016-10-18 15:17:28 +0200

    transformer functions from Entity to DTO

---
+++ pms-core/src/main/java/by/get/pms/utility/Transformers.java
@@ -0,0 +1,30 @@
+package by.get.pms.utility;
+
+import by.get.pms.dto.ProjectDTO;
+import by.get.pms.dto.TaskDTO;
+import by.get.pms.dto.UserDTO;
+import by.get.pms.model.Project;
+import by.get.pms.model.Task;
+import by.get.pms.model.User;
+
+import java.util.function.Function;
+
+/**
+ * Created by Milos.Savic on 10/18/2016.
+ */
+public class Transformers {
+
+	public static final Function<User, UserDTO> USER_ENTITY_2_USER_DTO_TRANSFORMER = user -> new UserDTO(user.getId(),
+			user.getFirstName(), user.getLastName(), user.getEmail(), user.getAssignedTasks(),
+			user.getUserAccount().getUsername(), user.getUserAccount().getCreationDate(),
+			user.getUserAccount().isActive(), user.getUserAccount().getRole().getCode());
+
+	public static final Function<Project, ProjectDTO> PROJECT_ENTITY_2_PROJECT_DTO_TRANSFORMER = project -> new ProjectDTO(
+			project.getId(), project.getCode(), project.getName(), project.getDescription(),
+			USER_ENTITY_2_USER_DTO_TRANSFORMER.apply(project.getProjectManager()));
+
+	public static final Function<Task, TaskDTO> TASK_ENTITY_2_TASK_DTO_FUNCTION = task -> new TaskDTO(task.getId(),
+			task.getName(), task.getTaskStatus(), task.getProgress(), task.getDeadline(), task.getDescription(),
+			USER_ENTITY_2_USER_DTO_TRANSFORMER.apply(task.getAssignee()),
+			PROJECT_ENTITY_2_PROJECT_DTO_TRANSFORMER.apply(task.getProject()));
+}
