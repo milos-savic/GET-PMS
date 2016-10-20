5145f7b2050e32ba90b2f17e34265dcee58af4d3
milos-savic <savic.losmi@gmail.com>
2016-10-20 22:41:20 +0200

    Validator Factory bean to create JSR-303 validators for @Validated beans

--- dist/pms-webapp/src/main/java/by/get/pms/config/WebMvcConfig.java
+++ dist/pms-webapp/src/main/java/by/get/pms/config/WebMvcConfig.java
@@ -1,9 +1,13 @@
 package by.get.pms.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -15,24 +19,35 @@ import java.util.Locale;
  * Created by milos.savic on 10/5/2016.
  */
 @Configuration
-@ComponentScan({ "by.get.pms.web" })
+@ComponentScan({"by.get.pms.web"})
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
-	@Bean
-	public LocaleResolver localeResolver() {
-		return new FixedLocaleResolver(Locale.US);
-	}
+    @Autowired
+    private MessageSource messageSource;
 
-	/**
-	 * Handles favicon.ico requests assuring no <code>404 Not Found</code> error
-	 * is returned.
-	 */
-	@Controller
-	static class FaviconController {
+    @Bean
+    public Validator validator() {
+        LocalValidatorFactoryBean validator = new LocalValidatorFactoryBean();
+        validator.setValidationMessageSource(messageSource);
+        return validator;
+    }
 
-		@RequestMapping("favicon.ico")
-		String favicon() {
-			return "forward:/resources/images/favicon.ico";
-		}
-	}
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new FixedLocaleResolver(Locale.US);
+    }
+
+    /**
+     * Handles favicon.ico requests assuring no <code>404 Not Found</code> error
+     * is returned.
+     */
+    @Controller
+    static class FaviconController {
+
+        @RequestMapping("favicon.ico")
+        String favicon() {
+            return "forward:/resources/images/favicon.ico";
+        }
+    }
 }
