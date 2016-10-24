4d497d59b581800ee39c49b1aa947d8bba7cc18e
Milos Savic <milos.savic@seavus.com>
2016-10-24 09:54:30 +0200

    UserDTO in application session instead of entity User

--- dist/pms-webapp/src/main/java/by/get/pms/security/Application.java
+++ dist/pms-webapp/src/main/java/by/get/pms/security/Application.java
@@ -1,6 +1,6 @@
 package by.get.pms.security;
 
-import by.get.pms.model.UserAccount;
+import by.get.pms.dto.UserDTO;
 import by.get.pms.model.UserRole;
 
 import java.time.LocalDateTime;
@@ -10,92 +10,80 @@ import java.util.Locale;
  * Entity containing application session attributes.
  */
 public class Application {
-    private ApplicationAttributes attributes;
+	private ApplicationAttributes attributes;
 
-    private LocalDateTime created;
-    private UserAccount userAccount;
-    private UserRole currentRole;
-    private by.get.pms.model.User user;
+	private LocalDateTime created;
+	//private UserAccount userAccount;
+	private UserRole currentRole;
+	private UserDTO user;
 
-    public Application(ApplicationAttributes attributes) {
-        if (attributes == null) {
-            throw new IllegalArgumentException("ApplicationAttributes must be specified");
-        }
-        this.attributes = attributes;
-        this.created = getAttribute("created");
-        this.userAccount = getAttribute("userAccount");
-        if (userAccount != null) {
-            this.user = userAccount.getUser();
-        }
-        this.currentRole = getAttribute("currentRole");
-    }
+	public Application(ApplicationAttributes attributes) {
+		if (attributes == null) {
+			throw new IllegalArgumentException("ApplicationAttributes must be specified");
+		}
+		this.attributes = attributes;
+		this.created = getAttribute("created");
+		this.user = getAttribute("user");
+		this.currentRole = getAttribute("currentRole");
+	}
 
-    public <T> T getAttribute(String key) {
-        ApplicationAttributes applicationAttributes = getApplicationAttributes();
-        return (T) applicationAttributes.get(key);
-    }
+	public <T> T getAttribute(String key) {
+		ApplicationAttributes applicationAttributes = getApplicationAttributes();
+		return (T) applicationAttributes.get(key);
+	}
 
-    public <T> void setAttribute(String key, T value) {
-        ApplicationAttributes applicationAttributes = getApplicationAttributes();
-        applicationAttributes.put(key, value);
-    }
+	public <T> void setAttribute(String key, T value) {
+		ApplicationAttributes applicationAttributes = getApplicationAttributes();
+		applicationAttributes.put(key, value);
+	}
 
-    protected ApplicationAttributes getApplicationAttributes() {
-        return attributes;
-    }
+	protected ApplicationAttributes getApplicationAttributes() {
+		return attributes;
+	}
 
-    public static Application getInstance() {
-        return new Application(ApplicationSecurityContext.getContext());
-    }
+	public static Application getInstance() {
+		return new Application(ApplicationSecurityContext.getContext());
+	}
 
-    public LocalDateTime getCreated() {
-        return created;
-    }
+	public LocalDateTime getCreated() {
+		return created;
+	}
 
-    public by.get.pms.model.User getUser() {
-        return user;
-    }
+	public UserDTO getUser() {
+		return user;
+	}
 
-    public UserAccount getUserAccount() {
-        return userAccount;
-    }
+	public UserRole getCurrentRole() {
+		return currentRole;
+	}
 
-    public UserRole getCurrentRole() {
-        return currentRole;
-    }
+	public void setCurrentRole(UserRole currentRole) {
+		this.currentRole = currentRole;
+		setAttribute("currentRole", currentRole);
+	}
 
-    public void setCurrentRole(UserRole currentRole) {
-        this.currentRole = currentRole;
-        setAttribute("currentRole", currentRole);
-    }
+	public Locale getLocale() {
+		return Locale.ENGLISH;
+	}
 
-    public Locale getLocale() {
-        return Locale.ENGLISH;
-    }
+	public void setCreated(LocalDateTime created) {
+		this.created = created;
+		setAttribute("created", created);
+	}
 
-    public void setCreated(LocalDateTime created) {
-        this.created = created;
-        setAttribute("created", created);
-    }
+	public void setUser(UserDTO user) {
+		this.user = user;
+		setAttribute("user", user);
+	}
 
-    public void setUserAccount(UserAccount userAccount) {
-        this.userAccount = userAccount;
-        if (userAccount != null) {
-            this.user = userAccount.getUser();
-        } else {
-            user = null;
-        }
-        setAttribute("userAccount", userAccount);
-    }
+	public void setCredentials(UserDTO user, UserRole currentRole) {
+		clearState();
+		setUser(user);
+		setCurrentRole(currentRole);
+		setCreated(LocalDateTime.now());
+	}
 
-    public void setCredentials(UserAccount userAccount, UserRole currentRole) {
-        clearState();
-        setUserAccount(userAccount);
-        setCurrentRole(currentRole);
-        setCreated(LocalDateTime.now());
-    }
-
-    protected void clearState() {
-        attributes.clearAll();
-    }
+	protected void clearState() {
+		attributes.clearAll();
+	}
 }
--- dist/pms-webapp/src/main/java/by/get/pms/security/AuthorizationServiceImpl.java
+++ dist/pms-webapp/src/main/java/by/get/pms/security/AuthorizationServiceImpl.java
@@ -3,6 +3,7 @@ package by.get.pms.security;
 import by.get.pms.dataaccess.UserAccountRepository;
 import by.get.pms.model.UserAccount;
 import by.get.pms.model.UserRole;
+import by.get.pms.utility.Transformers;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,68 +32,71 @@ import java.util.Set;
 @Transactional
 public class AuthorizationServiceImpl implements AuthorizationService {
 
-    protected static final Log LOGGER = LogFactory.getLog(AuthorizationServiceImpl.class);
+	protected static final Log LOGGER = LogFactory.getLog(AuthorizationServiceImpl.class);
 
-    @Autowired
-    private UserAccountRepository userAccountRepository;
+	@Autowired
+	private UserAccountRepository userAccountRepository;
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserAccount userAccount = userAccountRepository.findUserAccountByUsername(username);
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		UserAccount userAccount = userAccountRepository.findUserAccountByUsername(username);
 
-        if (userAccount != null && !(userAccount.isActive())) {
-            throw new LockedException("User account is locked");
-        }
+		if (userAccount != null && !(userAccount.isActive())) {
+			throw new LockedException("User account is locked");
+		}
 
-        if (userAccount == null) {
-            throw new UsernameNotFoundException("No user account registered in the system");
-        }
-        UserDetails userDetails = createUserDetails(userAccount);
+		if (userAccount == null) {
+			throw new UsernameNotFoundException("No user account registered in the system");
+		}
+		UserDetails userDetails = createUserDetails(userAccount);
 
-        final List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>(userDetails.getAuthorities());
-        if (authorities.size() == 0) {
-            throw new CredentialsExpiredException("No active role available");
-        }
-        return userDetails;
-    }
+		final List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>(userDetails.getAuthorities());
+		if (authorities.size() == 0) {
+			throw new CredentialsExpiredException("No active role available");
+		}
+		return userDetails;
+	}
 
-    private UserDetails createUserDetails(UserAccount userAccount) {
-        boolean accountNonExpired = true;
-        boolean isCredentialsNotExpired = true;
-        final Set<GrantedAuthority> granted = grantAuthorities(userAccount);
-        boolean isActive = userAccount.isActive();
+	private UserDetails createUserDetails(UserAccount userAccount) {
+		boolean accountNonExpired = true;
+		boolean isCredentialsNotExpired = true;
+		final Set<GrantedAuthority> granted = grantAuthorities(userAccount);
+		boolean isActive = userAccount.isActive();
 
-        final User user = new User(userAccount.getUsername(), null, isActive, accountNonExpired,
-                isCredentialsNotExpired, isActive, granted);
-        user.setDisplayName(userAccount.getUser().getFirstName() + " " + userAccount.getUser().getLastName());
-        return user;
-    }
+		final User user = new User(userAccount.getUsername(), null, isActive, accountNonExpired,
+				isCredentialsNotExpired, isActive, granted);
+		user.setDisplayName(userAccount.getUser().getFirstName() + " " + userAccount.getUser().getLastName());
+		return user;
+	}
 
-    @Override
-    public Set<GrantedAuthority> grantAuthorities(UserAccount userAccount) {
-        final Set<GrantedAuthority> granted = new HashSet<GrantedAuthority>();
+	@Override
+	public Set<GrantedAuthority> grantAuthorities(UserAccount userAccount) {
+		final Set<GrantedAuthority> granted = new HashSet<GrantedAuthority>();
 
-        final UserRole currentRole = userAccount.getRole();
+		final UserRole currentRole = userAccount.getRole();
 
-        if (currentRole != null) {
-            Application.getInstance().setCredentials(userAccount, currentRole);
-            granted.add(new SimpleGrantedAuthority(currentRole.name()));
-        }
+		if (currentRole != null) {
 
-        return granted;
-    }
+			Application.getInstance()
+					.setCredentials(Transformers.USER_ENTITY_2_USER_DTO_TRANSFORMER.apply(userAccount.getUser()),
+							currentRole);
+			granted.add(new SimpleGrantedAuthority(currentRole.name()));
+		}
 
-    @Override
-    public void onApplicationEvent(ApplicationEvent applicationEvent) {
-        if (applicationEvent instanceof AuthenticationSuccessEvent) {
+		return granted;
+	}
 
-            AuthenticationSuccessEvent event = (AuthenticationSuccessEvent) applicationEvent;
-            final String userName = event.getAuthentication().getName();
-            LOGGER.info("User " + userName + " has been successfully authenticated");
-        } else if (applicationEvent instanceof AuthorizationFailureEvent) {
-            AuthorizationFailureEvent event = (AuthorizationFailureEvent) applicationEvent;
-            final String userName = ((AuthorizationFailureEvent) applicationEvent).getAuthentication().getName();
-            LOGGER.info("Unauthorized access to" + event.getSource() + " has been detected.");
-        }
-    }
+	@Override
+	public void onApplicationEvent(ApplicationEvent applicationEvent) {
+		if (applicationEvent instanceof AuthenticationSuccessEvent) {
+			AuthenticationSuccessEvent event = (AuthenticationSuccessEvent) applicationEvent;
+			final String userName = event.getAuthentication().getName();
+			LOGGER.info(String.format("User %s has been successfully authenticated.", userName));
+		} else if (applicationEvent instanceof AuthorizationFailureEvent) {
+			AuthorizationFailureEvent event = (AuthorizationFailureEvent) applicationEvent;
+			final String userName = ((AuthorizationFailureEvent) applicationEvent).getAuthentication().getName();
+			LOGGER.info(
+					String.format("Unauthorized access to %s has been detected by %s.", event.getSource(), userName));
+		}
+	}
 }
