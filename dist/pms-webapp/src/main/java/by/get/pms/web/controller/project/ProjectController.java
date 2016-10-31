a30becf9a65f284f726b93b928f8cbb04e8e50de
milos-savic <savic.losmi@gmail.com>
2016-10-31 22:54:43 +0100

    comment authorization

--- dist/pms-webapp/src/main/java/by/get/pms/web/controller/project/ProjectController.java
+++ dist/pms-webapp/src/main/java/by/get/pms/web/controller/project/ProjectController.java
@@ -4,9 +4,9 @@ import by.get.pms.dto.EntitlementDTO;
 import by.get.pms.dto.ObjectType;
 import by.get.pms.dto.ProjectDTO;
 import by.get.pms.dto.UserDTO;
-import by.get.pms.security.Application;
 import by.get.pms.service.entitlement.EntitlementService;
 import by.get.pms.service.project.ProjectFacade;
+import by.get.pms.service.user.UserService;
 import by.get.pms.web.controller.WebConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -24,26 +24,34 @@ import java.util.stream.Collectors;
 @Controller
 public class ProjectController {
 
-	@Autowired
-	private ProjectFacade projectFacade;
+    @Autowired
+    private ProjectFacade projectFacade;
 
-	@Autowired
-	private EntitlementService entitlementService;
+    @Autowired
+    private EntitlementService entitlementService;
 
-	@RequestMapping(value = WebConstants.PROJECTS_URL, method = RequestMethod.GET)
-	public ModelAndView getProjects() {
-		ModelAndView modelAndView = new ModelAndView(WebConstants.PROJECTS_HTML_PATH);
-		List<ProjectDTO> projects = retrieveProjects(Application.getInstance().getUser());
-		modelAndView.getModel().put("projects", projects);
+    @Autowired
+    private UserService userService;
 
-		return modelAndView;
-	}
+    @RequestMapping(value = WebConstants.PROJECTS_URL, method = RequestMethod.GET)
+    public ModelAndView getProjects() {
+        ModelAndView modelAndView = new ModelAndView(WebConstants.PROJECTS_HTML_PATH);
 
-	private List<ProjectDTO> retrieveProjects(UserDTO user) {
-		List<EntitlementDTO> entitlementsForProjectsPermittedToUser = entitlementService
-				.getEntitlementsForObjectTypePermittedToUser(user.getUserName(), ObjectType.PROJECT);
-		Set<Long> projectIds = entitlementsForProjectsPermittedToUser.parallelStream().map(EntitlementDTO::getObjectid)
-				.collect(Collectors.toSet());
-		return projectFacade.getProjectByIds(projectIds);
-	}
+        //List<ProjectDTO> projects = retrieveProjects(Application.getInstance().getUser());
+        List<ProjectDTO> projects = retrieveProjects(userService.getUser(10L));
+
+        modelAndView.getModel().put("projects", projects);
+
+        // TODO: add projectManagers to model
+
+        return modelAndView;
+    }
+
+    private List<ProjectDTO> retrieveProjects(UserDTO user) {
+        List<EntitlementDTO> entitlementsForProjectsPermittedToUser = entitlementService
+                .getEntitlementsForObjectTypePermittedToUser(user.getUserName(), ObjectType.PROJECT);
+        Set<Long> projectIds = entitlementsForProjectsPermittedToUser.parallelStream().map(EntitlementDTO::getObjectid)
+                .collect(Collectors.toSet());
+        return projectFacade.getProjectByIds(projectIds);
+    }
 }
