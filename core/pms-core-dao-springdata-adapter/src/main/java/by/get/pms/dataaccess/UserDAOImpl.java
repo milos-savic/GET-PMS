ceb0afe1876d1916b045d801e9f5de212f560dec
milos-savic <savic.losmi@gmail.com>
2017-02-22 20:19:38 +0100

    moving spring data repos from adapter to spring-jpa component where they actually belong

--- core/pms-core-dao-springdata-adapter/src/main/java/by/get/pms/dataaccess/UserDAOImpl.java
+++ core/pms-core-dao-springdata-adapter/src/main/java/by/get/pms/dataaccess/UserDAOImpl.java
@@ -3,6 +3,7 @@ package by.get.pms.dataaccess;
 import by.get.pms.data.UserData;
 import by.get.pms.model.User;
 import by.get.pms.model.UserAccount;
+import by.get.pms.model.UserRole;
 import by.get.pms.springdata.UserAccountRepository;
 import by.get.pms.springdata.UserRepository;
 import by.get.pms.utility.Transformers;
@@ -21,81 +22,81 @@ import java.util.stream.Collectors;
 @Repository
 class UserDAOImpl implements UserDAO {
 
-	@Autowired
-	private UserAccountRepository userAccountRepository;
+    @Autowired
+    private UserAccountRepository userAccountRepository;
 
-	@Autowired
-	private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-	@Override
-	public boolean exists(Long userId) {
-		return userRepository.exists(userId);
-	}
+    @Override
+    public boolean exists(Long userId) {
+        return userRepository.exists(userId);
+    }
 
-	@Override
-	public boolean userExistsByUserName(String username) {
-		return userRepository.userExistsByUserName(username) > 0;
-	}
+    @Override
+    public boolean userExistsByUserName(String username) {
+        return userRepository.userExistsByUserName(username) > 0;
+    }
 
-	@Override
-	public List<UserData> findAll() {
-		List<User> users = Lists.newArrayList(userRepository.findAll());
+    @Override
+    public List<UserData> findAll() {
+        List<User> users = Lists.newArrayList(userRepository.findAll());
 
-		return users.parallelStream().map(Transformers.USER_ENTITY_2_USER_DATA_TRANSFORMER::apply)
-				.collect(Collectors.toList());
-	}
+        return users.parallelStream().map(Transformers.USER_ENTITY_2_USER_DATA_TRANSFORMER::apply)
+                .collect(Collectors.toList());
+    }
 
-	@Override
-	public UserData findOne(Long userId) {
-		User user = userRepository.findOne(userId);
-		return Transformers.USER_ENTITY_2_USER_DATA_TRANSFORMER.apply(user);
-	}
+    @Override
+    public UserData findOne(Long userId) {
+        User user = userRepository.findOne(userId);
+        return Transformers.USER_ENTITY_2_USER_DATA_TRANSFORMER.apply(user);
+    }
 
-	@Override
-	public UserData getUserByUserName(String username) {
-		UserAccount userAccount = userAccountRepository.findUserAccountByUsername(username);
+    @Override
+    public UserData getUserByUserName(String username) {
+        UserAccount userAccount = userAccountRepository.findUserAccountByUsername(username);
 
-		if (userAccount == null)
-			return null;
+        if (userAccount == null)
+            return null;
 
-		User user = userAccount.getUser();
-		return Transformers.USER_ENTITY_2_USER_DATA_TRANSFORMER.apply(user);
-	}
+        User user = userAccount.getUser();
+        return Transformers.USER_ENTITY_2_USER_DATA_TRANSFORMER.apply(user);
+    }
 
-	@Override
-	public UserData create(UserData userParams) {
-		User newUser = new User();
-		BeanUtils.copyProperties(userParams, newUser);
-		newUser = userRepository.save(newUser);
+    @Override
+    public UserData create(UserData userParams) {
+        User newUser = new User();
+        BeanUtils.copyProperties(userParams, newUser);
+        newUser = userRepository.save(newUser);
 
-		UserAccount newUserAccount = new UserAccount();
-		BeanUtils.copyProperties(userParams, newUserAccount, "creationDate");
-		newUserAccount.setUser(newUser);
-		newUserAccount.setRole(userParams.getRole());
-		newUserAccount.setCreationDateTime(LocalDateTime.now());
-		newUserAccount = userAccountRepository.save(newUserAccount);
+        UserAccount newUserAccount = new UserAccount();
+        BeanUtils.copyProperties(userParams, newUserAccount, "creationDate");
+        newUserAccount.setUser(newUser);
+        newUserAccount.setRole(UserRole.make(userParams.getRole().name()));
+        newUserAccount.setCreationDateTime(LocalDateTime.now());
+        newUserAccount = userAccountRepository.save(newUserAccount);
 
-		newUser.setUserAccount(newUserAccount);
+        newUser.setUserAccount(newUserAccount);
 
-		return Transformers.USER_ENTITY_2_USER_DATA_TRANSFORMER.apply(newUser);
-	}
+        return Transformers.USER_ENTITY_2_USER_DATA_TRANSFORMER.apply(newUser);
+    }
 
-	@Override
-	public void update(UserData userParams) {
-		User userFromDb = userRepository.findOne(userParams.getId());
-		BeanUtils.copyProperties(userParams, userFromDb);
+    @Override
+    public void update(UserData userParams) {
+        User userFromDb = userRepository.findOne(userParams.getId());
+        BeanUtils.copyProperties(userParams, userFromDb);
 
-		UserAccount userAccountFromDb = userAccountRepository.findUserAccountByUser(userFromDb);
-		BeanUtils.copyProperties(userParams, userAccountFromDb, "id", "creationDate");
-		userAccountFromDb.setRole(userParams.getRole());
-	}
+        UserAccount userAccountFromDb = userAccountRepository.findUserAccountByUser(userFromDb);
+        BeanUtils.copyProperties(userParams, userAccountFromDb, "id", "creationDate");
+        userAccountFromDb.setRole(UserRole.make(userParams.getRole().name()));
+    }
 
-	@Override
-	public void delete(Long userId) {
-		User userFromDb = userRepository.findOne(userId);
-		UserAccount userAccount = userAccountRepository.findUserAccountByUser(userFromDb);
-		userAccountRepository.delete(userAccount);
+    @Override
+    public void delete(Long userId) {
+        User userFromDb = userRepository.findOne(userId);
+        UserAccount userAccount = userAccountRepository.findUserAccountByUser(userFromDb);
+        userAccountRepository.delete(userAccount);
 
-		userRepository.delete(userId);
-	}
+        userRepository.delete(userId);
+    }
 }
--- core/pms-core-dao-springdata-adapter/src/main/java/by/get/pms/utility/Transformers.java
+++ core/pms-core-dao-springdata-adapter/src/main/java/by/get/pms/utility/Transformers.java
@@ -1,8 +1,6 @@
 package by.get.pms.utility;
 
-import by.get.pms.data.ProjectData;
-import by.get.pms.data.TaskData;
-import by.get.pms.data.UserData;
+import by.get.pms.data.*;
 import by.get.pms.model.Project;
 import by.get.pms.model.Task;
 import by.get.pms.model.User;
@@ -14,17 +12,17 @@ import java.util.function.Function;
  */
 public final class Transformers {
 
-	public static final Function<User, UserData> USER_ENTITY_2_USER_DATA_TRANSFORMER = user -> new UserData(
-			user.getId(), user.getFirstName(), user.getLastName(), user.getEmail(), user.getUserAccount().getUserName(),
-			user.getUserAccount().getCreationDateTime(), user.getUserAccount().isActive(),
-			user.getUserAccount().getRole());
+    public static final Function<User, UserData> USER_ENTITY_2_USER_DATA_TRANSFORMER = user -> new UserData(
+            user.getId(), user.getFirstName(), user.getLastName(), user.getEmail(), user.getUserAccount().getUserName(),
+            user.getUserAccount().getCreationDateTime(), user.getUserAccount().isActive(),
+            UserRole.make(user.getUserAccount().getRole().name()));
 
-	public static final Function<Project, ProjectData> PROJECT_ENTITY_2_PROJECT_DATA_TRANSFORMER = project -> new ProjectData(
-			project.getId(), project.getCode(), project.getName(), project.getDescription(),
-			USER_ENTITY_2_USER_DATA_TRANSFORMER.apply(project.getProjectManager()));
+    public static final Function<Project, ProjectData> PROJECT_ENTITY_2_PROJECT_DATA_TRANSFORMER = project -> new ProjectData(
+            project.getId(), project.getCode(), project.getName(), project.getDescription(),
+            USER_ENTITY_2_USER_DATA_TRANSFORMER.apply(project.getProjectManager()));
 
-	public static final Function<Task, TaskData> TASK_ENTITY_2_TASK_DATA_TRANSFORMER = task -> new TaskData(task.getId(),
-			task.getName(), task.getTaskStatus(), task.getProgress(), task.getDeadline(), task.getDescription(),
-			task.getAssignee() != null ? USER_ENTITY_2_USER_DATA_TRANSFORMER.apply(task.getAssignee()) : null,
-			PROJECT_ENTITY_2_PROJECT_DATA_TRANSFORMER.apply(task.getProject()));
+    public static final Function<Task, TaskData> TASK_ENTITY_2_TASK_DATA_TRANSFORMER = task -> new TaskData(task.getId(),
+            task.getName(), TaskStatus.make(task.getTaskStatus().name()), task.getProgress(), task.getDeadline(), task.getDescription(),
+            task.getAssignee() != null ? USER_ENTITY_2_USER_DATA_TRANSFORMER.apply(task.getAssignee()) : null,
+            PROJECT_ENTITY_2_PROJECT_DATA_TRANSFORMER.apply(task.getProject()));
 }
